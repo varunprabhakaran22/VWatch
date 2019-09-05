e998c2299c1c07b8e048dcf1d2824e8a0ca03c5c
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-05 12:41:58 +0530

    design is in process

---
+++ script.js
@@ -0,0 +1,33 @@
+function StopWatch(){
+
+    var startTime = null; 
+    var stopTime = null; 
+    var running = false; 
+  }
+
+  function getTime(){
+    var day = new Date();
+    return day.getTime();
+  }
+
+  this.start = function(){ 
+
+    if (running == true)
+        return;
+    else if (startTime != null) 
+        stopTime = null; 
+    
+    running = true;    
+    startTime = getTime();
+    
+    }
+    this.stop = function(){ 
+
+        if (running == false)
+            return;    
+        
+        stopTime = getTime();
+        running = false; 
+        
+        }
+        
