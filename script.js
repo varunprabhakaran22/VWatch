ba76046e8ed4aad57b1cf20788bb22254e5c92e4
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-18 21:25:38 +0530

    creating the clock

--- script.js
+++ script.js
@@ -84,24 +84,24 @@ function getTime(){
 
 
 
-// function add() {
-//   seconds++;
-//   if (seconds >= 60) {
-//       seconds = 0;
-//       minutes++;
-//       if (minutes >= 60) {
-//           minutes = 0;
-//           hours++;
-//       }
-//   }
-//   h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+function add() {
+  seconds++;
+  if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
+          minutes = 0;
+          hours++;
+      }
+  }
+  h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
-//   timer();
-// }
-// function timer() {
-//     t = setTimeout(add, 1000);
-// }
-// timer();
+  timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
 
 // /* Start button */
 // start.onclick = timer;
